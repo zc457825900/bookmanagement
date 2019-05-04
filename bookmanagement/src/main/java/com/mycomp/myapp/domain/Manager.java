5ddaa00c241684396173d9af913f03df2bfba32a
Little know zhang <457825900@qq.com>
2019-05-04 23:10:39 +0800

    添加图书管çç®书管理员类â

---
+++ bookmanagement/src/main/java/com/mycomp/myapp/domain/Manager.java
@@ -0,0 +1,12 @@
+package com.mycomp.myapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 图书管理员
+ */
+@Entity
+@Table(name="bm_manager")
+public class Manager {
+}
